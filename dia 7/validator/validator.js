c696748557db6183a46c45f62d1b792838136791
Simon Lara <simon.larah@gmail.com>
2019-09-11 12:32:52 -0300

    j

---
+++ dia 7/validator/validator.js
@@ -0,0 +1,25 @@
+
+
+
+let form = document.getElementById('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log("Submit");
+   
+    let messaje=document.getElementById('messaje');
+    if (messaje.value=="" || validator.isLowercase('holA')==false){
+        let alerta = document.getElementById('alerta');
+    
+        alerta.classList.add('mostrar');
+        alerta.innerHTML="Falta mensaje";
+    } else {
+        alerta.classList.remove('mostrar');
+        e.target.submit();
+    }
+
+});
+
+
+console.log(validator.isLowercase('holA'));
+
+
